c3b349c452e2d6e188691159c77fd16ce6f6229c
Anderson Misson <missondeveloper@gmail.com>
2020-07-09 11:51:31 -0300

    Criando um component book-data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BookDataComponent } from './book-data/book-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookDataComponent
   ],
   imports: [
     BrowserModule,
